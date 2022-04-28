797c32f92d105850bfadc1dce3009a37e4bcd059
Fabio Mendes <fabiomendes53@gmail.com>
2022-04-27 21:10:21 -0300

    feat: initial database configurations

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => {
-    res.status(200).send('Teste backend.')
-})
 
 app.listen(3000, () => {
     console.log('Aplicação em execução...')
